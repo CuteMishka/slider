b6ee029d221d2dc2e828a4063dcddc10fb1da3c4
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-08-18 16:55:22 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var lenta = document.getElementById('lenta')
 let decrease = -100
 function plus() {
-	decrease = -100 - -(decrease)
+	decrease = -100 + decrease
 	lenta.style.left = decrease + 'px'
+	if (decrease < -300) {decrease=100}
 }
